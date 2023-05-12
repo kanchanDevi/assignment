4f4bfe5759f69178acee7b23b989f0cbb37b974f
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-05-12 21:51:52 +0530

    assignment

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -21,7 +21,7 @@ const Facts = () => {
     <div className=" bg-slate-50 flex flex-col justify-center items-center h-[100vh] w-[100vw] p-5">
     <p className="p-2 text-xl">Fact: {post.fact}</p>
     <p className='p-2'>Length: {post.length}</p>
-    <button onClick={() => window.location.reload(false)} className='bg-blue-600 p-4 rounded text-white weight-bold'>Click to reload!</button>
+    <button onClick={() => window.location.reload(false)} className='bg-blue-600 p-4 rounded text-white weight-bold'>Refresh</button>
     </div>
     </>
   )
